587f9b07cdff761ec07eea10f122bdeffe7ec895
ho3eintry <ho3intry@gmail.com>
2023-05-07 16:45:12 +0330

    finish CRUD with gi-gonic

--- frameworks/gin-web/main.go
+++ frameworks/gin-web/main.go
@@ -21,6 +21,55 @@ func main() {
 		c.JSON(http.StatusOK, AllBooks())
 	})
 
+	// create new book
+	engine.POST("/api/books", func(c *gin.Context) {
+		var book Book
+		if c.BindJSON(&book) == nil {
+			isbn, created := CreateBook(book)
+			if created {
+				c.Header("Location", "/api/books"+isbn)
+				c.Status(http.StatusCreated)
+			} else {
+				c.Status(http.StatusConflict)
+			}
+		}
+	})
+
+	// get books with ISBN
+	engine.GET("/api/books/:isbn", func(c *gin.Context) {
+		isbn := c.Params.ByName("isbn")
+		book, found := GetBook(isbn)
+		if found {
+			c.JSON(http.StatusOK, book)
+		} else {
+			c.AbortWithStatus(http.StatusNotFound)
+		}
+	})
+
+	//update books by ISBN
+	engine.PUT("/api/books:isbn", func(c *gin.Context) {
+		isbn := c.Params.ByName("isbn")
+
+		var book Book
+
+		if c.BindJSON(&book) == nil {
+			exists := UpdateBook(isbn, book)
+			if exists {
+				c.Status(http.StatusOK)
+			} else {
+				c.Status(http.StatusNotFound)
+			}
+		}
+	})
+
+	// delete book
+	engine.DELETE("/api/books/:isbn", func(c *gin.Context) {
+		isbn := c.Params.ByName("isbn")
+		DeleteBook(isbn)
+		c.Status(http.StatusOK)
+	})
+
+	// run server on PORT
 	engine.Run(port())
 }
 
